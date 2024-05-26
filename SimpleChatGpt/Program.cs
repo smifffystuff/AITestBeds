f126d0aba502efd3201cf707cf7a5968fdf947eb
Martin <martin.smifff@gmail.com>
2024-05-26 17:44:12 +0100

    Implement chatbot with history

--- SimpleChatGpt/Program.cs
+++ SimpleChatGpt/Program.cs
@@ -7,3 +7,47 @@ builder.AddOpenAIChatCompletion("gpt-3.5-turbo", Environment.GetEnvironmentVaria
 
 var kernel = builder.Build();
 
+var prompt = @"
+You are a Chatbot and a user can have a conversation with you about any topic.
+It can give explicit information or say 'I don't know' if it doesn't have an answer.
+
+{{$history}}
+User: {{$userInput}}
+ChatBot:";
+
+var chatFunction = kernel.CreateFunctionFromPrompt(prompt,
+    executionSettings: new OpenAIPromptExecutionSettings
+    {
+        MaxTokens = 2000,
+        Temperature = 0.7,
+        TopP = 0.5
+    });
+
+var arguments = new KernelArguments();
+var history = "";
+
+Console.WriteLine("Hi, I am a ChatBot ask me anything!");
+
+while (true)
+{
+    var userInput = Console.ReadLine();
+    if (userInput.Trim().ToLower() == "exit")
+    {
+        break;
+    }
+    Func<string, Task> Chat = async (string input) =>
+    {
+        arguments["userInput"] = userInput;
+
+        var botAnswer = await chatFunction.InvokeAsync(kernel, arguments);
+
+        var result = $"\nUser: {userInput}\nAI: {botAnswer}\n";
+        history += result;
+        arguments["history"] = history;
+
+        Console.WriteLine(botAnswer);
+    };
+
+    await Chat(userInput!);
+    
+}
